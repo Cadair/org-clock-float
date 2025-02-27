021f4e15efedcc3e5264e6a89f574bec45f23a06
Stuart Mumford <stuart@cadair.com>
2025-02-27 12:02:44 +0000

    meta data

--- org-clock-float.el
+++ org-clock-float.el
@@ -1,5 +1,33 @@
 ;;; ...  -*- lexical-binding: t -*-
-;; Send clocked times to float.com
+;;; org-clock-float.el --- Summary
+
+;; Copyright (C) 2025 Stuart Mumford
+
+;; Author: Stuart Mumford <stuart@cadair.com>
+;; Keywords: org, clocking, float
+;; Version: 1.0
+;; Package-Requires: ((emacs "26.1") (request "0.3.2")
+;; URL: https://github.com/Cadair/org-clock-float/
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Clock the active task on float.com upon checkout.
+;;
+
+;;; Code:
 (require 'request)
 (require 'json)
 (require 'org-clock)
